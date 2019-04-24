2ca13690df1b2462f2204d0cac133702f7fc61b2
taniaks <35944188+taniaks@users.noreply.github.com>
2019-04-23 21:38:25 -0700

    top module calling the des block

--- topmoduleTripleDes.v
+++ topmoduleTripleDes.v
@@ -1,1 +1,19 @@
+module tripledes (output reg [63:0] password, input clk, e, s,[55:0] key1, key2, [63:0] intext);
+reg [63:0] in, out;
+genvar d;
+generate
+in = intext;
+e = 1'b1;
+	for (d=1; d<=3; d=d+1) begin
+		if (s==1|s==3) begin
+		des dm (.out, .in, .s, .key1, .e, .clk);
+		in = out;
+		end
+		else begin
+		des dm1 (.out, .in, .s, .key2, .e, .clk);
+		in = out;
+		end
+	end
+endgenerate
+endmodule
 
