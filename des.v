3904f1600441b01afb82942d4f1b88fe48ac7fca
taniaks <35944188+taniaks@users.noreply.github.com>
2019-04-23 22:28:56 -0700

    fixed the loop output

--- des.v
+++ des.v
@@ -6,7 +6,7 @@ reg [55:0] kp;
 generate
 if (e) begin   //this is the encryption order
 	p_function kp (.in(key1), .out(kp));  //need to pass a parameter setting it to 56 bits!!!!
-	for (t=1; t<=16; t=t+1) begin
+	for (t=1; t<=16; t=t+1) begin  //repeats it 16 times just like specified in the algorithm
 	expansion ex (.in(in[63:32]), .out(exp));    //expands to 48
 	keyMixer km (.in(kc), .nextkey(ks), .newkey(kp));  //pass to key mixer which breaks shifts does func and returns a 48 bit change key
 	kp= ks;
@@ -15,6 +15,7 @@ if (e) begin   //this is the encryption order
 	p_function tp (.in(sr), .out(pr)); //this one is dealing with 32 bits, need parameter
 	out[63:32]= pr ^ in[31:0];
 	out[31:0]= in[63:32];
+	in = out;
 	end
 	s=s+1'b1;
 	e=~e;
