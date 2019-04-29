7f2dc86a641ef65f1a70143c09d9bce89effd456
Vince-Trien Ngo <trien.ngo559@gmail.com>
2019-04-29 09:08:56 -0700

    expansion block done (needs testing though)
    testbench is next to be made for the expansion block

--- expansion.v
+++ expansion.v
@@ -1,3 +1,27 @@
 module expansion(input [31:0] in, output [47:0] out);
-    assign out = 48'b1;
+    reg [7:0] bit8one;
+    reg [7:0] bit8two;
+    reg [7:0] bit8three;
+    reg [7:0] bit8four;
+
+    reg [9:0] bit10one;
+    reg [9:0] bit10two;
+    reg [9:0] bit10three;
+    reg [9:0] bit10four;
+
+    initial begin
+        assign bit8one = in[7:0];
+        assign bit8two = in[15:8];
+        assign bit8three = in[23:16];
+        assign bit8four = in[31:24];
+        
+        assign bit10one = {in[31],bit8one,in[8]};
+        assign bit10two = {in[7],bit8two,in[16]};
+        assign bit10three = {in[15],bit8three,in[24]};
+        assign bit10four = {in[23],bit8one,in[0]};
+
+    end
+
+    assign out = {bit10one,bit10two,bit10three,bit10four};
+
 endmodule
