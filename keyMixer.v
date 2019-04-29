622f3a3842d26e4b9a23cf011d3e6db3a60c839f
taniaks <35944188+taniaks@users.noreply.github.com>
2019-04-29 15:04:17 -0700

    fixed stuff

--- keyMixer.v
+++ keyMixer.v
@@ -1,9 +1,17 @@
-module keyMixer (output [55:0] nextkey, newkey, input [55:0] in);
+module keyMixer (output [55:0] nextkey, newkey, input [4:0] t, [55:0] in);
   reg [27:0] a, b;
-  initial begin
-    assign a =in[27:0] << 1;
-    assign b =in[55:28] << 1;
+  reg [1:0] l;
+  always @(*) begin
+    if (t==4'b0001|t==4'b0010|t==4'b01001|t==4'b10000) begin
+      l=2'b01;
+      a =in[27:0] <<< l;
+      b =in[55:28] <<< l; end
+    else begin
+      l=2'b10;
+      a =in[27:0] <<< l;
+      b =in[55:28] <<< l; end
+    nextkey = {b,a};
+    //change p into a function and put here
   end
-  assign nextkey = {b,a};
   p_function #(56, 48) pf (.in(nextkey), .out(newkey));
 endmodule
