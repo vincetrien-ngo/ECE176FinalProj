68770c5e9a40141ce0d4aa8bc8db7ee0daa5824e
taniaks <35944188+taniaks@users.noreply.github.com>
2019-04-28 22:09:59 -0700

    not sure if we should use this or procedural

--- keyMixer.v
+++ keyMixer.v
@@ -1,1 +1,7 @@
-
+module keyMixer (output [55:0] nextkey, newkey, input [55:0] in);
+reg [27:0] a, b;
+assign a =in[27:0] << 1;
+assing b =in[55:28] << 1;
+assing nextkey = {b,a};
+p_function #(56, 56) pf (.in(nextkey), .out(newkey));
+endmodule
