03c00ee82329dd7cdf71c0c6ef4f2b2e90c79975
Brian Cardwell <bcardwell86@gmail.com>
2019-04-30 09:45:32 -0700

    Update s_function.v

--- s_function.v
+++ s_function.v
@@ -1,15 +1,15 @@
 module s_function(input [47:0] in, output reg [31:0] out);
-// need to import funtions s1-s8 once finished
+
 	
 	always@(in) begin
-		// S1
-		out[31:28] <= s1(in[47:42]);
-		out[27:24] <= s2(in[41:36]);
-		out[23:20] <= s3(in[35:30]);
-		out[19:16] <= s4(in[29:24]);
-		out[15:12] <= s5(in[23:18]);
-		out[11:8] <= s6(in[17:12]);
-		out[7:4] <= s7(in[11:6]);
-		out[3:0] <= s8(in[5:0]);
+		
+		s1(out[31:28], in[47:42]);
+		s2(out[27:24], in[41:36]);
+		s3(out[23:20], in[35:30]);
+		s4(out[19:16], in[29:24]);
+		s5(out[15:12], in[23:18]);
+		s6(out[11:8], in[17:12]);
+		s7(out[7:4], in[11:6]);
+		s8(out[3:0], in[5:0]);
 
 endmodule		
