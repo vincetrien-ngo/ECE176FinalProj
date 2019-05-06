607fa3d2df60fb4a048c286db90936a26445731b
Brian Cardwell <bcardwell86@gmail.com>
2019-05-06 12:27:18 -0700

    updated newkey

--- keyMixer.v
+++ keyMixer.v
@@ -1,15 +1,12 @@
-module keyMixer (output [55:0] nextkey, newkey, input [4:0] t, [55:0] in);
+module keyMixer (output reg [55:0] nextkey, output [47:0] newkey, input [4:0] t, [55:0] in);
   reg [27:0] a, b;
-  reg [1:0] l;
   always @(*) begin
-    if (t==4'b0001|t==4'b0010|t==4'b01001|t==4'b10000) begin
-      l=2'b01;
-      a =in[27:0] <<< l;
-      b =in[55:28] <<< l; end
+    if (t==4'b0001|t==4'b0010|t==4'b1001|t==4'b1000) begin
+      a =in[27:0] <<< 1;
+      b =in[55:28] <<< 1; end
     else begin
-      l=2'b10;
-      a =in[27:0] <<< l;
-      b =in[55:28] <<< l; end
+      a =in[27:0] <<< 2;
+      b =in[55:28] <<< 2; end
     nextkey = {b,a};
     //change p into a function and put here
   end
