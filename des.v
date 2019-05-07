6afb166af9f5bfbb366492d19ddf724bbfb53e4f
taniaks <35944188+taniaks@users.noreply.github.com>
2019-05-06 20:12:10 -0700

    fixed key size

--- des.v
+++ des.v
@@ -1,4 +1,4 @@
-module des (output reg [63:0] out, input e, [63:0] k, [63:0] in);	
+module des (output reg [63:0] out, input e, [55:0] k, [63:0] in);	
 always @(e) begin
 if (e)    //this is the encryption order
 	encrypt(out, k, in);
