14ce6ec440ef99293c83cfc49dcea36e21dec7f8
taniaks <35944188+taniaks@users.noreply.github.com>
2019-05-06 19:22:28 -0700

    Got rid of round now this basically selects parts

--- desRounds.v
+++ desRounds.v
@@ -1,8 +1,7 @@
-module desRounds(output reg [31:0] new_L, new_R, [15:0] new_round, input [63:0] R_L_input, [15:0] round);
+module desRounds(output reg [31:0] new_L, new_R, input [63:0] R_L_input);
     initial begin
         new_L = R_L_input[31:0];
         new_R = R_L_input[63:32];
 
-        new_round = round + 1;
     end
 endmodule
