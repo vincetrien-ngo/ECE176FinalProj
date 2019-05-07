0c2e8ffecb63464c8e03aa104414067fffe84507
Vince-Trien Ngo <trien.ngo559@gmail.com>
2019-05-06 20:28:11 -0700

    changed titles

---
+++ equals.v
@@ -0,0 +1,8 @@
+module equals(output reg [55:0] out, input [55:0] in);
+    initial begin
+        assign out = in;
+    end
+endmodule
+
+
+
--- equals_kp_ks.v
+++
@@ -1,7 +0,0 @@
-module equals_kb_ks(output reg [55:0] kp, input [55:0] ks);
-    initial begin
-        assign kp = ks;
-    end
-endmodule
-
-
