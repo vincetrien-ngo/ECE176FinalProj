90b9579f047d19ba88fcef16976acd4395dc1004
taniaks <35944188+taniaks@users.noreply.github.com>
2019-05-06 22:43:36 -0700

    module

--- encrypt.v
+++ encrypt.v
@@ -1,14 +1,15 @@
-task encrypt;
-output reg [63:0] out;
-input [63:0] in;
-input [55:0] k;
-    reg [47:0] exp, is, kc;
-    reg [31:0] sr, pr, R_i, L_i;
-    reg [55:0] kp, ks;
-    wire [31:0] wL_i;
-    wire [63:0] w_in;
-	 begin
-    integer t;
+module encrypt(
+output reg [63:0] out,
+input [63:0] in,
+[55:0] k);
+
+reg [47:0] exp, is, kc;
+reg [31:0] sr, pr, R_i, L_i;
+reg [55:0] kp, ks;
+wire [31:0] wL_i;
+wire [63:0] w_in;
+genvar t;
+generate
 		p_function #(64, 56, 4) kper (.in(k), .out(kp));  //need to pass a parameter setting it to 56 bits!!!!
 		p_function #(64, 64, 0) init (.in(in), .out(w_in));
 		desRounds unot0(.new_L(L_i),.new_R(R_i),.R_L_input(w_in));
@@ -22,9 +23,7 @@ input [55:0] k;
 			s_function st (.in(is), .out(sr));
 			p_function #(32, 32, 3) tp (.in(sr), .out(pr)); //this one is dealing with 32 bits, need parameter
 			expon u3(.out(R_i),.thisto(pr),.that(wL_i));
-
 		end
 		p_function #(64, 64, 1) inv_init (.in({R_i,L_i}), .out(out));
-		end
-endtask
-
+endgenerate 
+endmodule
