f9be7803d40ac4bbbd52b212decf1b9139e0ca99
taniaks <35944188+taniaks@users.noreply.github.com>
2019-05-06 19:20:53 -0700

    wrote

--- encrypt.v
+++ encrypt.v
@@ -1,8 +1,7 @@
-		module encrypt (output reg [63:0] out, input [63:0] k, [63:0] in);
+module encrypt (output reg [63:0] out, input [63:0] k, [63:0] in);
     reg [47:0] exp, is, kc;
     reg [31:0] sr, pr, R_i, L_i;
     reg [55:0] kp, ks;
-    reg [15:0] round;
 
     wire [31:0] wL_i;
     wire [63:0] w_in;
@@ -10,48 +9,20 @@
 		generate 
 		p_function #(64, 56, 4) kper (.in(k), .out(kp));  //need to pass a parameter setting it to 56 bits!!!!
 		p_function #(64, 64, 0) init (.in(in), .out(w_in));
-		desRounds unot0(
-			.new_L(L_i)			, 
-			.new_R(R_i)			, 
-			.R_L_input(w_in)	,
-			.round(round)
-		);
-		// R_i = w_in[31:0];
-		// L_i = w_in[63:32];
-		// round = 0;
+		desRounds unot0(.new_L(L_i),.new_R(R_i),.R_L_input(w_in));
 
 		for (t=1; t<=16; t=t+1) begin  //repeats it 16 times just like specified in the algorithm
-			// round = round + 1;
-			// wL_i = L_i;
-			// L_i = R_i;
-			desRounds u0(
-				.new_L(wL_i)			, 
-				.new_R(L_i)				, 
-				.R_L_input({L_i,R_i})	,
-				.round(round)
-			);
+			desRounds u0(.new_L(wL_i)	,.new_R(L_i),.R_L_input({L_i,R_i}),.round(round));
 			expansion ex (.in(R_i), .out(exp));    //expands to 48
 			keyMixer km (.in(kp), .nextkey(ks), .newkey(kc), .t(round));  //pass to key mixer which breaks shifts does func and returns a 48 bit change key
-			equals u1(
-				.out(kp)	, 
-				.in(ks)
-			);// kp= ks;
-			expon u2(
-				.out(is)		,
-				.thisto(exp)	,
-				.that(kc)		
-			);//is= exp ^ kc;    //xor of the changed key and expanded part of text
+			equals u1(.out(kp),.in(ks));// kp= ks;
+			expon u2(.out(is),.thisto(exp),.that(kc));//is= exp ^ kc;    //xor of the changed key and expanded part of text
 			s_function st (.in(is), .out(sr));
 			p_function #(32, 32, 3) tp (.in(sr), .out(pr)); //this one is dealing with 32 bits, need parameter
-			expon u3(
-				.out(R_i)		,
-				.thisto(pr)		,
-				.that(wL_i)		
-			);
-			//R_i = pr ^ wL_i;
+			expon u3(.out(R_i),.thisto(pr),.that(wL_i));
+
 		end
-		//s=s+1'b1; // s is the start signal, it tells the topmodule how many iterations have been done and keeps trak of which key to use. 
-		//e=~e;
 		p_function #(64, 64, 1) inv_init (.in({R_i,L_i}), .out(out));
 		endgenerate
 endmodule
+
