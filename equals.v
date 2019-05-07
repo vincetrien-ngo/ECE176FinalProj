8f7c04f16695d76411dde9245443bd7e19862b52
Brian Cardwell <bcardwell86@gmail.com>
2019-05-07 11:38:09 -0700

    simulates

--- equals.v
+++ equals.v
@@ -1,7 +1,6 @@
-module equals(out, in);
-parameter N=56;
-output [N-1:0] out;
-inout [N-1:0] in;
+module equals #(parameter N=56)(output [N-1:0] out, input [N-1:0] in);
+
+
 assign out = in;
 endmodule
 
--- topmoduleTripleDes.v
+++ topmoduleTripleDes.v
@@ -1,4 +1,4 @@
-module tripledes (output reg [63:0] password, input clk, e,[55:0] key1, key2, [63:0] intext);
+module tripledes (output reg [63:0] password, input clk, e,[63:0] key1, key2, [63:0] intext);
 wire [63:0]  out;
 
 
@@ -8,11 +8,11 @@ generate
 		if ((d==1|d==3)) 
 		begin
 			des em (.out, .in(intext), .k(key1), .e);
-			equals u0(.out(intext),.in(out));//in = out;
+			equals #(64) u0(.out(intext),.in(out));//in = out;
 		end
 		else begin
 				des em1 (.out, .in(intext), .k(key2), .e);
-				equals u6(.out(intextt), .in(out));//in = out;
+				equals #(64) u6(.out(intext), .in(out));//in = out;
 		end
 	end
 endgenerate
