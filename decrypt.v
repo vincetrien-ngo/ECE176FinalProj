4cb1fe63b76b23161818686faec64bc7ca0e8f15
taniaks <35944188+taniaks@users.noreply.github.com>
2019-05-07 19:47:47 -0700

    put in forloop

--- decrypt.v
+++ decrypt.v
@@ -15,7 +15,7 @@ generate
 		p_function #(64, 64, 0) init (.in(in), .out(w_in));
 		desRounds unot0(.new_L(L_i),.new_R(R_i),.R_L_input(w_in));
 
-		//for (t=1; t<=16; t=t+1) begin  //repeats it 16 times just like specified in the algorithm
+		for (t=1; t<=16; t=t+1) begin  //repeats it 16 times just like specified in the algorithm
 			desRounds u0(.new_L(wL_i)	,.new_R(L_i2),.R_L_input({L_i,R_i}));
 			expansion ex (.in(R_i), .out(exp));    //expands to 48
 			keyMixer km (.in(kp), .nextkey(ks), .newkey(kc), .t(round));  //pass to key mixer which breaks shifts does func and returns a 48 bit change key
@@ -24,7 +24,7 @@ generate
 			s_function st (.in(is), .out(sr));
 			p_function #(32, 32, 3) tp (.in(sr), .out(pr)); //this one is dealing with 32 bits, need parameter
 			expon #(32) u3(.out(R_i2),.in1(pr),.in2(wL_i));
-		//end
+		end
 		p_function #(64, 64, 1) inv_init (.in({R_i2,L_i2}), .out(tempout));
 endgenerate 
 always @ (*) out <= tempout;
